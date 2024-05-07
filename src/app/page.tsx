e762de4e3e236f79734adb2377980e48e7f4b816
nirojneupane5 <nirojneupane5@gmail.com>
2024-05-07 08:28:09 +0545

    Profile page added with image and some basic information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
+import profile from "../../public/profile/Niroj Neupane.jpg";
 export default function Home() {
   return (
-    <div className="h-screen">
-      <div className="text-black">This is niroj neupane portfolio</div>
+    <div className="h-96 flex justify-center items-center">
+      <div>
+        <Image
+          src={profile}
+          alt="Niroj Neupane profile"
+          width={200}
+          height={200}
+          className="rounded-full"
+        />
+      </div>
+      <div className="ml-10">
+        <h1 className="text-xl font-bold text-black">Hello</h1>
+        <h1 className="text-2xl font-bold text-black">I'm Niroj Neupane</h1>
+        <h1 className="text-2xl font-bold text-black">Fullstack Developer</h1>
+      </div>
     </div>
   );
 }
