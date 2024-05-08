3eafd5a9e29b74864a7536f934e7a34d5685fb2f
nirojneupane5 <nirojneupane5@gmail.com>
2024-05-08 19:42:18 +0545

    changes in navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import { Navbar } from "@/Components/Navbar";
+import { Navbar } from "./(Components)/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
