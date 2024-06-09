77c61be03fac58db6c390bb2f3be67810851416d
nirojneupane5 <nirojneupane5@gmail.com>
2024-06-09 13:10:10 +0545

    Added profile pic and text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,28 @@
+import Layout from "@/components/Layout";
+import profilepic from "../../public/profile/Niroj Neupane.jpg";
+import Image from "next/image";
 export default function Home() {
-  return <>Home</>;
+  return (
+    <main className="flex items-center text-dark w-full min-h-screen">
+      <Layout className="pt-0">
+        <div className="flex items-center justify-between w-full">
+          <div className="w-1/2">
+            <Image
+              src={profilepic}
+              alt="profile pic"
+              className="object-cover w-full h-auto rounded-full"
+            />
+          </div>
+          <div className="w-1/2">
+            <h1>Turning Vision Into Reality With Code And Design</h1>
+            <p>
+              As a skilled full-stack developer, I am dedicated to turning ideas
+              into innovative web applications. Explore my latest projects and
+              articles, showcasing my expertise in React.js and web development.
+            </p>
+          </div>
+        </div>
+      </Layout>
+    </main>
+  );
 }
