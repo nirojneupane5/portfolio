a727d92215fbf0a9e4debd7696039678d2c3b108
nirojneupane5 <nirojneupane5@gmail.com>
2024-06-10 10:48:20 +0545

    Hire me added with Circular text

---
+++ src/components/HireMe.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import CircularText from "./Icon";
+
+const HireMe = () => {
+  return (
+    <div className="fixed left-4 bottom-4 flex items-center justify-center overflow-hidden">
+      <div className="w-full h-auto flex items-center justify-center relative">
+        <CircularText className={"fill-black animate-spin-slow"} />
+        <Link
+          href="mailto:nirojneupane5@gmial.com"
+          className="flex items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black text-white shadow-md border border-solid border-black h-20 w-20 rounded-full hover:text-black hover:bg-white font-semibold"
+        >
+          Hire Me!!
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default HireMe;
