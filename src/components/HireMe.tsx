604f2242f466f2018ec15fb1f8e0f7ca847564f4
nirojneupane5 <nirojneupane5@gmail.com>
2024-06-11 09:15:05 +0545

    About page biography

--- src/components/HireMe.tsx
+++ src/components/HireMe.tsx
@@ -8,7 +8,7 @@ const HireMe = () => {
         <CircularText className={"fill-black animate-spin-slow"} />
         <Link
           href="mailto:nirojneupane5@gmial.com"
-          className="flex items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black text-white shadow-md border border-solid border-black h-20 w-20 rounded-full hover:text-black hover:bg-white font-semibold"
+          className="flex items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black text-white shadow-md border border-solid border-black h-20 w-20 rounded-full hover:text-black hover:bg-white font-semibold duration-300"
         >
           Hire Me!!
         </Link>
